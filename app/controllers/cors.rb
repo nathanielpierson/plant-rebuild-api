dea30ded85ceba00ba10feb2a29ea8f085aaaf6e
nathanielpierson <nathanielepierson@gmail.com>
2025-06-01 16:05:00 -0400

    add cors

---
+++ app/controllers/cors.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "localhost:5173", "plant-capstone-frontend.onrender.com"
+    resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
+  end
+end
