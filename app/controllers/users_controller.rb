2f9c4cf4bd045ad3997849c5b9f64a9098d71908
nathanielpierson <nathanielepierson@gmail.com>
2025-06-01 15:50:58 -0400

    add user scaffold

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render :index
+  end
+  def show
+    @user = User.find_by(id: current_user.id)
+    render :show
+  end
+  def create
+    user = User.new(
+      name: params[:name],
+      email: params[:email],
+      image_url: params[:image_url],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+end
