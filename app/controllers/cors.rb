8a87a6e9852f37dd367018e8e60bb407ce88f53e
nathanielpierson <nathanielepierson@gmail.com>
2025-06-03 20:38:40 -0400

    corsS

--- app/controllers/cors.rb
+++ app/controllers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "plant-capstone-frontend.onrender.com"
+    origins "*"
     resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
   end
 end
