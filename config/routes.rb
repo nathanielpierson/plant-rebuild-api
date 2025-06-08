02f9f63943fa2d2135758a4e4e4f47e125b65ee0
nathanielpierson <nathanielepierson@gmail.com>
2025-06-07 21:08:39 -0400

    add routes info

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :plants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +11,29 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/users" => "users#index"
+  get "/users/current" => "users#show"
+  post "/users" => "users#create"
+
+  get "/plants" => "plants#index"
+  get "/plants/:id" => "plants#show"
+  post "/plants" => "plants#create"
+  patch "/plants/:id" => "plants#update"
+  delete "/plants/:id" => "plants#delete"
+
+  get "/schedules" => "schedules#index"
+  get "/schedules/plant/:id" => "schedules#show_of_plant"
+  get "/schedules/:id" => "schedules#show"
+  post "/schedules" => "schedules#create"
+  put "/schedules" => "schedules#update"
+  put "schedules/:id/water" => "schedules#water"
+  delete "/schedules/:id" => "schedules#delete"
+  patch "/reset" => "schedules#reset"
+
+  get "/plant_counts" => "plant_counts#index"
+  put "/plant_counts/:id" => "plant_counts#update"
+
+  post "/sessions" => "sessions#create"
+
+  post "/test" => "plant_counts#create"
 end
